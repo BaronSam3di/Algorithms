9d4fa71a9624c0830cd23c93f190ca4694b34f88
Geoffrey Owden <geoffrey.owden@ibm.com>
2021-02-08 21:44:25 +0000

    Bubble

---
+++ Easy/BubbleSort.py
@@ -0,0 +1,37 @@
+"""
+----- Bubble Sort : Easy -----
+------ BRIEF ------
+Write a function that tales in an array of integers and returns a sorted version of that array. use the Bubble Sort algorithm to sort the array.
+
+The goal is to sort a list of numbers in assending order. 
+On each iteration we will swap if  the number on right of the current number larger than the current number ;ie are they sorted.
+If they are not in the correct order, we will swap the number in place ( we are not useing a helper array like other sort algos).
+
+
+------ Hints ------
+
+
+------ Complexity ------ 
+Space = O(1) because it is done in place, not storage needed
+Time = O(n^2) could be at worst 
+
+------ Recursive Formula ------
+
+------ Iterative Approach ------
+
+"""
+## Time o(n^2) | Space O(1)  
+def bubbleSort(array):
+    isSorted = False
+    counter = 0    #  Very small optimization 
+    while not isSorted:
+        isSorted = True
+        for i in range(len(array) - 1 - counter):
+            if array[i] > array[i + 1]:
+                swap(i, i + 1 , array)
+                isSorted = False
+        counter += 1
+    return array
+
+def swap(i, j , array):
+    array[i], array[j] = array[j], array[i]     # putting this in the main function is better for python 
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Must know for any interview
 Easy:
 - Depth First search is a 
 - Fibonacci 
+
+Tip 
+- Start timing myself half way through my prep calender
+- aim to spend max 40 mins on a problem
