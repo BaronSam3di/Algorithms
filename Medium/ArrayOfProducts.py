5d5fe10be3ef4d81066cc4256283e3567d44cc38
Geoffrey Owden <geoffrey.owden@ibm.com>
2021-02-28 18:11:10 +0000

    test

---
+++ Medium/ArrayOfProducts.py
@@ -0,0 +1,57 @@
+"""
+----- Array Of Products  : Medium -----
+------ BRIEF ------
+
+Write a function that tales in a non-empty array of integers and returns an array of 
+the same length, where each element in the output array is equal to the product of every other number in the input array.
+
+In other words , the value at `output[i]` is equal to the product of every number in the input array other than input[i]
+
+>>>> Note that you're expected to solve this problem WITHOUT USING DIVISION. <<<<
+
+values can be + . - pr zero.
+
+eg; [5,1,4,2] will result in [8,40,10,20]. Index 0 is 8 because 1 x 4 x 2 = 8 , and so on. 
+------ Hints ------
+- Think about the most naive approach to solving this problem. 
+How can we do exactly what the problem wants us to do without focusing at all time and space complexity? 
+
+- Understand how output[i] is being calculated. 
+How can we calculate the product of every element other than the one at the current index? 
+Can we do this with just one loop through the input array, or do we have to do multiple loops?
+
+- For each index in the input array, try calculateing the product of every element to the left and the product of every element to the right.
+You can do this with two loops through the array: one from left to right and one from right to left. How can these products help us?
+
+------ Complexity ------ 
+Brute force: Space = O(n) , Time - On^2)
+Optimal : Space - O(n) , Time = O(n) where n is the length of the arrya. 
+
+------ Approach ------
+
+"""
+## Brute force approach
+## Space = O(n), Time - O(n^2)
+def arrayOfProducts(array):
+    products = [1 for _ in range(len(array))]      # fill it wit hvalues of 1's to start
+
+    for i in range(len(array)):
+        runningProduct = 1
+        for j in range(len(array)):
+            if i != j:                  # except for the index of the outer traversal current element , add the products
+                runningProduct *= array[j]
+        products[i] = runningProduct
+    
+    return products
+
+
+
+
+
+def arrayOfProducts(array):
+    products = [1 for _ in range(len(array))]      # fill it with values of 1's to start
+
+    leftProducts = [1 for _ in range(len(array))] 
+    rightProducts = [1 for _ in range(len(array))] 
+
+    return products
