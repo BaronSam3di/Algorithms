13f65061d73f16e3f5f03e2094e0c8cecbaed9de
Geoffrey Owden <geoffrey.owden@ibm.com>
2021-03-13 19:43:23 +0000

    stair

--- Medium/StarcaseTraversal.py
+++ Medium/StarcaseTraversal.py
@@ -29,11 +29,88 @@ if the height == 0 or 1:
 return FUNC(h-1) + FUNC(h-2) + ... +FUNC(h-s) # where s is the max steps
 
 ------ Solution 2 ------
-
+Time: O(n) | Space: O(n)
 Use memoisation : Cache the results to save time and space.
 Time: O(k*n) - where K is the max number of steps we can take ;and n ih the height.
 ------ Iterative Approach ------
-
 """
 
 ####################################################
+## Solution 1:  O(k^n) Time | (O(n) Space
+####################################################
+
+def startcaseTraversal(height, maxSteps):
+    return numberOfWaysToTop(height, maxSteps)
+
+def numberOfWaysToTop(height, maxSteps):
+    if height <=1 :
+        return 1
+    
+    numberOfWays = 0
+    for step in range(1,min(maxSteps, height) +1 ):          # The min function is there for when the the height is smaller than the max steps
+        numberOfWays += numberOfWaysToTop(height - step, maxSteps)
+    
+    return numberOfWays
+
+
+
+####################################################
+## Solution 2 :O(k * n) Time | (O(n) Space
+####################################################
+# As above but with Memoisation
+
+def startcaseTraversal(height, maxSteps):
+    return numberOfWaysToTop(height, maxSteps, { 0:1, 1:1 })        # adding the memoise base case
+
+def numberOfWaysToTop(height, maxSteps, memoize):                   # memoise is the cache
+    if height in memoize:
+        return memoize[height]                                      # return the existing awnser
+    
+    numberOfWays = 0
+    for step in range(1,min(maxSteps, height) +1 ):          # The min function is there for when the the height is smaller than the max steps
+        numberOfWays += numberOfWaysToTop(height - step, maxSteps)
+
+    memoize[height] = numberOfWays
+    
+    return numberOfWays
+
+####################################################
+## Solution 3 :O(k * n) Time | (O(n) Space
+####################################################
+# As above but with Memoisation
+
+def startcaseTraversal(height, maxSteps):
+    waysToTop = [0 for _ in range(height + 1)]          # initialize data structure for cache
+    waysToTop[0] = 1
+    waysToTop[1] = 1
+
+    for currentHeight in range (2, height + 1):
+        # this is where we do the k work
+        step = 1
+        while step <= maxSteps and step <= currentHeight:
+            # below line looks backward in data structure and see how the previous were solved and add that
+            waysToTop[currentHeight] = waysToTop[currentHeight] + waysToTop[currentHeight + step] 
+            step += 1
+
+        return waysToTop[height]  
+
+
+####################################################
+## Solution 4: O(k * n) Time | (O(n) Space
+####################################################
+# As above but with Memoisation
+
+def startcaseTraversal(height, maxSteps):
+    currentNumberOfWays = 0 
+    waysToTop = [1]
+
+    for currentHeight in range(1, height + 1):
+        startOfWindow = currentHeight - maxSteps - 1        # calculating the number we need to subtract . We want the value from the previous window
+        endOfWindow = currentHeight - 1
+        if startOfWindow >= 0:                              
+            currentNumberOfWays -= waysToTop[startOfWindow]  
+
+        currentNumberOfWays += waysToTop[endOfWindow]       # This is the new value we are adding
+        waysToTop.append(currentNumberOfWays)
+
+    return waysToTop[height]
